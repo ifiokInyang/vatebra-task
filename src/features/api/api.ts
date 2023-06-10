8c0b66e4e91c0c57e8c4718553c7f1c5f4304d84
ifiokInyang <ifiok.inyang@decagon.dev>
2023-06-10 20:24:56 +0100

    set up redux

---
+++ src/features/api/api.ts
@@ -0,0 +1,12 @@
+import { apiRequest } from "../../utils/api/requestMethod";
+
+// export const login = async (dispatch: any, user: ILogin) => {
+//   dispatch(loginStart());
+//   try {
+//     const response = await apiRequest.post("/api/auth/login", user);
+//     console.log(response.data);
+//     dispatch(loginSuccess(response.data));
+//   } catch (error) {
+//     dispatch(loginFailure());
+//   }
+// };
